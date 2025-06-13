3eb02d34d572c6ace108c2e1ba7074ef59a65415
Danish Ali <dali27363@gmail.com>
2025-06-13 16:10:37 +0500

    fix: remove checkAdmin middleware from user registration and category management routes

--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ const { checkAdmin } = require("../middlewares/type");
 const categoryController = require("../controllers/categoryController");
 const adminRouter = express.Router();
 
-adminRouter.post("/register", checkAdmin, adminController.register);
+adminRouter.post("/register", adminController.register);
 adminRouter.post("/login", adminController.login);
 adminRouter.get("/session", adminMiddleware, adminController.getSession);
 adminRouter.get("/stats", adminMiddleware, adminController.getStats);
@@ -15,27 +15,23 @@ adminRouter.get('/users', adminMiddleware, adminController.getUsers);
 adminRouter.post(
   "/users/:status",
   adminMiddleware,
-  checkAdmin,
   adminController.updateUserStatus
 );
 adminRouter.post(
   "/credentials-status/:status",
   adminMiddleware,
-  checkAdmin,
   adminController.updateCredentialsStatus
 );
 
 adminRouter.post(
   "/categories",
   adminMiddleware,
-  checkAdmin,
   categoryController.createCategory
 );
 adminRouter.get('/categories', adminMiddleware, categoryController.getCategories);
 adminRouter.delete(
   "/categories/:id",
   adminMiddleware,
-  checkAdmin,
   categoryController.deleteCategory
 );
 
